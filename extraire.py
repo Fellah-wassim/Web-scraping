80a61214634469fe7758eb98d7f0e8c7d9ab9dc0
wassim <wbvb41@gmail.com>
2022-12-27 23:55:48 +0100

    fixed some issues on the script

--- extraire.py
+++ extraire.py
@@ -2,27 +2,27 @@ import sys
 from bs4 import BeautifulSoup
 import urllib.request
 
-srt1 = open("subst.dic",'w',encoding='utf-16-le')
-srt1.write('\ufeff')
-srt2 = open("infos1.txt",'w')
+subst = open("subst.dic",'w',encoding='utf-16-le')
+subst.write('\ufeff')
+infos1 = open("infos1.txt",'w')
 data = sys.argv[1] 
 page = data[0] 
 fin_page = data[2]
-port = sys.argv[2]
+userPort = sys.argv[2]
 i = 0
 nbr = 0
 while(ord(page) <= ord(fin_page)):
-    src = urllib.request.urlopen('http://127.0.0.1:'+ str(port) + '/vidal/vidal-Sommaires-Substances-'+page+'.htm')
+    src = urllib.request.urlopen('http://127.0.0.1:'+ str(userPort) + '/vidal/vidal-Sommaires-Substances-'+page+'.htm')
     soup = BeautifulSoup(src, 'lxml')
     match = soup.find('ul', class_='substances list_index has_children')
     match.text.strip()
     for medcine in match.text.splitlines():
         if not medcine.strip():
             continue
-        srt1.write(medcine+",.N+subst\n")
+        subst.write(medcine+",.N+subst\n")
         nbr = nbr+1
-    srt2.write("le nombre des mediacaments avec la lettre " + str(page)+" est: "+ str(nbr)+"\n")
+    infos1.write("le nombre des mediacaments avec la lettre " + str(page)+ " est: " + str(nbr)+ "\n")
     i = nbr+i
     nbr = 0
     page = chr(ord(page)+1)
-srt2.write("le nombre total des medicaments est :"+str(i))
+    infos1.write("le nombre total des medicaments est :" + str(i))
